97433b9ded972268cf71bf1b2dc40cf38a0461f9
faineance <faineance@users.noreply.github.com>
2016-02-28 21:51:24 +0000

    more stuff

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-const id = (o) => o;
 
-const compose = f => g => (...xs) => f(g.apply(this, xs))
+const name = (f) => f.name
 
-const composeMany = (...fns) => fns.reduce((f, g) => (...args) => f(g(...args)));
+const arity = (f) => f.length
 
 const curry = (fn, ...args) => {
   let _curry = (args) =>
@@ -13,4 +12,28 @@ const curry = (fn, ...args) => {
   return _curry(args);
 }
 
+const id = (o) => o;
+
+const constant = (o) => ( () => o )
+
+const not = (o) => !o
+
+const or = curry((a, b) => a || b)
+
+const and = curry((a, b) => a && b)
+
+const trampoline = (f) => {
+    while (f && f instanceof Function) {
+        f = f();
+    }
+    return f;
+}
+
+
+const compose = f => g => (...xs) => f(g.apply(this, xs))
+
+const composeMany = (...fns) => fns.reduce((f, g) => (...args) => f(g(...args)));
+
+
 export { id, compose, composeMany, curry }
+ 
