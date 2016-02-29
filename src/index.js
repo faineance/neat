295b5f8d91359d67e53c8112f6e43be0a44885cb
faineance <faineance@users.noreply.github.com>
2016-02-29 09:29:39 +0000

    more

--- src/index.js
+++ src/index.js
@@ -4,36 +4,26 @@ const name = (f) => f.name
 const arity = (f) => f.length
 
 const curry = (fn, ...args) => {
-  let _curry = (args) =>
-    args.length < fn.length ?
+  const _curry = (args) =>
+    arity(args) < arity(fn) ?
       (..._args) => _curry([...args, ..._args]) :
       fn(...args)
 
-  return _curry(args);
+  return _curry(args)
 }
 
 const id = (o) => o;
 
-const constant = (o) => ( () => o )
+const constant = (o) => () => o
 
 const not = (o) => !o
 
 const or = curry((a, b) => a || b)
 
-const and = curry((a, b) => a && b)
-
-const trampoline = (f) => {
-    while (f && f instanceof Function) {
-        f = f();
-    }
-    return f;
-}
-
+const and = curry((a, b) => a && b)  
 
 const compose = f => g => (...xs) => f(g.apply(this, xs))
 
-const composeMany = (...fns) => fns.reduce((f, g) => (...args) => f(g(...args)));
+const composeMany = (...fns) => fns.reduce((f, g) => (...args) => f(g(...args)))
 
-
-export { id, compose, composeMany, curry }
- 
+export { name, arity, curry, id, constant, not, or, and, compose, composeMany }
--- test/neat.spec.js
+++ test/neat.spec.js
@@ -7,28 +7,78 @@ const expect = chai.expect
 
 
 describe('Given an instance of my library', () => {
+  describe('name', () => {
 
-
-    describe('id', () => {
-
-        it('should return the parameter supplied to it', () => {
-            expect(neat.id(0)).to.be.equal(0)
-        })
+    it('should return the name of the function given', () => {
+      expect(neat.name(Math.pow)).to.be.equal("pow")
     })
-    describe('compose', () => {
+  })
 
-        it('should perform right to left function composition', () => {
-            let test = neat.compose(Math.sqrt)(Math.pow) 
-            expect(test(3,4)).to.be.equal(9) // sqrt(3^4)
-        })
-    })
-    describe('composeMany', () => {
+  describe('arity', () => {
 
-        it('should perform right to left function composition', () => {
-            let test = neat.compose(Math.sqrt)(Math.pow) 
-            expect(test(3,4)).to.be.equal(9) // sqrt(3^4)
-        })
+    it('should return the arity of the function given', () => {
+      expect(neat.arity(Math.pow)).to.be.equal(2)
     })
+  })
+
+  describe('curry', () => {
+
+    it('should return the curried version of the function given', () => {
+      let curried = neat.curry(Math.pow)
+      expect(curried(3)(4)).to.be.equal(81)
+    })
+  })
+
+  describe('id', () => {
+
+    it('should return the param supplied to it', () => {
+      expect(neat.id(0)).to.be.equal(0)
+    })
+  })
+
+  describe('constant', () => {
+
+    it('should return a function taking no params and return the param given', () => {
+      expect(neat.constant("Hello World")()).to.be.equal("Hello World")
+    })
+  })
+
+  describe('not', () => {
+
+    it('should perform the not operation on the given param', () => {
+      expect(neat.not(true)).to.be.equal(false)
+    })
+  })
+
+  describe('or', () => {
+
+    it('should perform the or operation on the given params', () => {
+      expect(neat.or(true)(false)).to.be.equal(true)
+    })
+  })
+
+  describe('and', () => {
+
+    it('should perform the and operation on the given params', () => {
+      expect(neat.and(true)(false)).to.be.equal(false)
+    })
+  })
+
+  describe('compose', () => {
+
+    it('should perform right to left function composition', () => {
+      let test = neat.compose(Math.sqrt)(Math.pow)
+      expect(test(3, 4)).to.be.equal(9) // sqrt(3^4)
+    })
+  })
+  
+  describe('composeMany', () => {
+
+    it('should perform right to left function composition', () => {
+      let test = neat.composeMany(Math.sqrt, Math.pow)
+      expect(test(3, 4)).to.be.equal(9) // sqrt(3^4)
+    })
+  })
 
 
 })
